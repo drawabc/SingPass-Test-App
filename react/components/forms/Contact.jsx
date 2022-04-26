83ba1fae6b5feaf9e8f84ae0a734f35736241e22
drawabc <edwardchristopher@ymail.com>
2022-04-26 23:40:38 +0800

    add backend template and form components

---
+++ react/components/forms/Contact.jsx
@@ -0,0 +1,100 @@
+const Contact = (props) => {
+  const register = props.register;
+  return (
+    <form>
+      <div className="mt-3">
+        <div className="h4">Personal Info</div>
+        <div className="mb-3">
+          <label for="nric" className="form-label">
+            NRIC
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="nric"
+            {...register("nric")}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            {...register("name")}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="sex" className="form-label">
+            Sex
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="sex"
+            {...register("sex")}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="dateOfBirth" className="form-label">
+            Date of Birth
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="dateOfBirth"
+            {...register("dateOfBirth")}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="country" className="form-label">
+            Country of Birth
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="country"
+            {...register("country")}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="residentialStatus" className="form-label">
+            Residential Status
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="residentialStatus"
+            {...register("residentialStatus")}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="nationality" className="form-label">
+            Nationality
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="nationality"
+            {...register("nationality")}
+          />
+        </div>
+        <div className="mb-3">
+          <label for="race" className="form-label">
+            Race
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="race"
+            {...register("race")}
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Contact;
